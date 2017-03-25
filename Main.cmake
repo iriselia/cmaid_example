e7f8cdd6882e1a6595d790273190f0b6cbba3c0a
Frank Park <fpark12@icloud.com>
2017-03-25 18:54:34 -0400

    ..

--- Main.cmake
+++ Main.cmake
@@ -1,5 +1,7 @@
 cmake_minimum_required( VERSION 2.8 )
 
+set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/Purify")
+
 include( Core/Utils )
 include( Core/SetOutputDirectories )
 include( Core/ProjectSettingsTemplate )
